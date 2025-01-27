c59d800a5a2b6d9ce52334a4a85de2417e16b579
benup <benup211@gmail.com>
2025-01-27 14:25:35 +0545

    feat(fix):file upload size fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,9 +20,9 @@ export class MainServer {
 
     async setConfiguration() {
         this.app.use(express.json({limit: '50mb'}));
-        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(express.urlencoded({ extended: true,limit: '50mb' }));
         this.app.use(cookieParser());
-        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.json({limit:'50mb'}));
         this.app.use(cors(
             {
                 origin: [process.env.ORIGIN_URL as string],
